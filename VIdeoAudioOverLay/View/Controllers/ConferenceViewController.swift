142ca6cf446f16fa565583bbd8102c4bd08ae324
Green Full Stack <greentree619@outlook.com>
2023-02-09 10:03:20 -0500

    Conference UI View

---
+++ VIdeoAudioOverLay/View/Controllers/ConferenceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ConferenceViewController.swift
+//  VIdeoAudioOverLay
+//
+//  Created by Kayan Mishra on 2/9/23.
+//
+
+import Foundation
+import UIKit
+
+class ConferenceViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
